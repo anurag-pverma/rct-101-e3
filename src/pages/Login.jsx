265feea347f3e399fda415501423eff4ada5ee03
Anurag Verma <anurag.premaverma@gmail.com>
2022-06-06 16:37:18 +0530

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,23 @@
 import React, { useState, useContext } from "react";
+import Navbar from "../components/Navbar/Navbar";
 import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [state, dispatch] = useContext(AppContext);
+
   return (
-    <div>
-      <input data-cy="login-email" />
-      <input data-cy="login-password" />
-      <button data-cy="login-submit"></button>
+    <>
+    <Navbar/>
+    <div >
+      <p>Login</p>
+      <input type="email" data-cy="login-email"  /><br />
+      <input type="password" data-cy="login-password" /> <br />
+      <button style={{width:"170px"}} type="submit" data-cy="login-submit">Login</button>
     </div>
+    </>
   );
 };
 
